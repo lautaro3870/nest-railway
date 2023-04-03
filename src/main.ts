d486bb0786506495ad8359ed4561e3e4cf20d49d
lautaro3870 <lautarodisbro@gmail.com>
2023-04-03 09:56:39 +0200

    Funcionará?

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = 3000
 
   const config = new DocumentBuilder()
     .setTitle('Documentacion Api')
@@ -14,6 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
   app.enableCors();
-  await app.listen(process.env.PORT);
+  await app.listen(PORT);
 }
 bootstrap();
